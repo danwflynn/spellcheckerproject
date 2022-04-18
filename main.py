02ff72f12122dfccc82d5d5af7663f3a0112909f
danwflynn <103793791+danwflynn@users.noreply.github.com>
2022-04-18 00:57:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,27 @@ sct = SpellChek("spellcheckertest.txt")
 
 print("Menu:\n"
       "1. Ignore a word\n"
-      "2. Print suggestions\n"
-      "3. Exit\n")
+      "2. Print suggestions and corrections\n"
+      "3. Refresh suggestions for a given word\n"
+      "4. Get corrections from user\n"
+      "5. Make corrections to document\n"
+      "6. Print the document\n"
+      "7. Exit\n")
 
 while menu:
-    choice = int(input("Input a number (1-3): "))
+    choice = int(input("Input a number (1-7): "))
     if choice == 1:
         sct.ignore_all(input("Ignore: "))
     elif choice == 2:
         print(sct)
-    else:
+    elif choice == 3:
+        sct.refresh_suggestions(input("Refresh: "))
+    elif choice == 4:
+        sct.get_corrections_from_user()
+    elif choice == 5:
+        sct.make_corrections()
+    elif choice == 6:
+        sct.print_doc()
+        print()
+    elif choice == 7:
         menu = False
--- spellchek.py
+++ spellchek.py
@@ -2,21 +2,25 @@
 from nltk.corpus import words
 import itertools
 import json
+import random
+import math
+
+
+def valid_word(w):
+    return w in words.words() \
+           or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("ed") in words.words() \
+           or w[0:len(w)-1] + w[len(w)-1].strip("s") in words.words() \
+           or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("'s") in words.words() \
+           or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("s'") in words.words() \
+           or w[0:len(w)-3] + w[len(w)-3:len(w)-1].strip("n't") in words.words()
 
 
 class SpellChek:
     def __init__(self, file):
-        def valid_word(w):
-            return w in words.words()\
-                   or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("ed") in words.words()\
-                   or w[0:len(w)-1] + w[len(w)-1].strip("s") in words.words()\
-                   or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("'s") in words.words()\
-                   or w[0:len(w)-2] + w[len(w)-2:len(w)-1].strip("s'") in words.words()\
-                   or w[0:len(w)-3] + w[len(w)-3:len(w)-1].strip("n't") in words.words()
-
         self.file = file
         self.text = open(file, "r", encoding='utf-8')
-        d = {}
+        self.suggestions = {}
+        self.corrections = {}
         for line in self.text:
             line = line.strip()
             line = line.lower()
@@ -43,44 +47,73 @@ class SpellChek:
                 word = word.strip("7")
                 word = word.strip("8")
                 word = word.strip("9")
-                if len(word) > 0 and not valid_word(word):
+                if 0 < len(word) < 3 and not valid_word(word):
                     fixes = []
-                    for x in [''.join(p) for p in list(itertools.permutations(list(word)))][1:5]:
+                    for x in [''.join(p) for p in list(itertools.permutations(list(word)))]:
                         if len(x) > 0 and valid_word(x):
                             fixes.append(x)
-                    d[word] = fixes
-                    if len(d[word]) < 5:
-                        for i in range(len(word)):
-                            z = list(word)
-                            z.pop(i)
-                            if len(d[word]) < 5 and len(z) > 0 and valid_word("".join(z)) and "".join(z) not in d[word]:
-                                d[word].append("".join(z))
-                                break
-                    if len(d[word]) < 5:
-                        for i in range(len(word)):
-                            z = list(word)
-                            z.insert(i+1, z[i])
-                            if len(d[word]) < 5 and len(z) > 0 and valid_word("".join(z)) and "".join(z) not in d[word]:
-                                d[word].append("".join(z))
-                                break
-                    if len(d[word]) < 5 and len(word) == 9:
+                    self.suggestions[word] = fixes
+                    if len(self.suggestions[word]) < 5:
                         for i in range(len(word)):
                             z = list(word)
                             z.insert(i+1, " ")
                             y = "".join(z).split()
-                            if len(d[word]) < 5 and len(z) > 0 and valid_word(y[0]) and valid_word(y[1])\
-                                    and " ".join(y) not in d[word]:
-                                d[word].append(" ".join(y))
-                    if len(d[word]) == 0:
+                            if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word(y[0])\
+                                    and valid_word(y[1]) and " ".join(y) not in self.suggestions[word]:
+                                self.suggestions[word].append(" ".join(y))
+                    if len(self.suggestions[word]) == 0:
                         vowels = ['a', 'e', 'i', 'o', 'u']
                         for i in range(len(word)):
                             for a in vowels:
                                 z = list(word)
                                 z[i] = a
-                                if len(d[word]) < 5 and len(z) > 0 and valid_word("".join(z))\
-                                        and "".join(z) not in d[word]:
-                                    d[word].append("".join(z))
-        self.suggestions = d
+                                if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z)) \
+                                        and "".join(z) not in self.suggestions[word]:
+                                    self.suggestions[word].append("".join(z))
+                elif len(word) > 0 and not valid_word(word):
+                    fixes = []
+                    for x in [''.join(p) for p in list(itertools.permutations(list(word)))][1:5]:
+                        if len(x) > 0 and valid_word(x):
+                            fixes.append(x)
+                    self.suggestions[word] = fixes
+                    if len(self.suggestions[word]) < 5:
+                        for i in range(len(word)):
+                            z = list(word)
+                            z.pop(i)
+                            if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z)) and "".join(z)\
+                                    not in self.suggestions[word]:
+                                self.suggestions[word].append("".join(z))
+                                break
+                    if len(self.suggestions[word]) < 5:
+                        for i in range(len(word)):
+                            z = list(word)
+                            z.insert(i+1, z[i])
+                            if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z)) and "".join(z)\
+                                    not in self.suggestions[word]:
+                                self.suggestions[word].append("".join(z))
+                                break
+                    if len(self.suggestions[word]) < 5 and len(word) > 8:
+                        for i in range(len(word)):
+                            z = list(word)
+                            z.insert(i+1, " ")
+                            y = "".join(z).split()
+                            if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word(y[0])\
+                                    and valid_word(y[1]) and " ".join(y) not in self.suggestions[word]:
+                                self.suggestions[word].append(" ".join(y))
+                    if len(self.suggestions[word]) == 0:
+                        vowels = ['a', 'e', 'i', 'o', 'u']
+                        for i in range(len(word)):
+                            for a in vowels:
+                                z = list(word)
+                                z[i] = a
+                                if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z)) \
+                                        and "".join(z) not in self.suggestions[word]:
+                                    self.suggestions[word].append("".join(z))
+
+    def print_doc(self):
+        with open(self.file, encoding='utf8') as f:
+            for line in f:
+                print(line, end='')
 
     def ignore_all(self, word):
         word = word.lower()
@@ -89,6 +122,87 @@ class SpellChek:
         else:
             print("Not found in suggestions")
 
+    def refresh_suggestions(self, word):
+        if word in self.suggestions.keys() and len(word) > 3:
+            fixes = []
+            r = random.randint(5, math.factorial(len(word)) - 5)
+            for x in [''.join(p) for p in list(itertools.permutations(list(word)))][r:r+5]:
+                if len(x) > 0 and valid_word(x):
+                    fixes.append(x)
+            self.suggestions[word] = fixes
+            if len(self.suggestions[word]) < 5:
+                for i in range(len(word)):
+                    z = list(word)
+                    z.pop(i)
+                    if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z))\
+                            and "".join(z) not in self.suggestions[word]:
+                        self.suggestions[word].append("".join(z))
+            if len(self.suggestions[word]) < 5:
+                for i in range(len(word)):
+                    z = list(word)
+                    z.insert(i + 1, z[i])
+                    if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z))\
+                            and "".join(z) not in self.suggestions[word]:
+                        self.suggestions[word].append("".join(z))
+            if len(self.suggestions[word]) < 5 and len(word) > 8:
+                for i in range(len(word)):
+                    z = list(word)
+                    z.insert(i + 1, " ")
+                    y = "".join(z).split()
+                    if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word(y[0]) and valid_word(y[1]) \
+                            and " ".join(y) not in self.suggestions[word]:
+                        self.suggestions[word].append(" ".join(y))
+            if len(self.suggestions[word]) < 5:
+                vowels = ['a', 'e', 'i', 'o', 'u']
+                for i in range(len(word)):
+                    for a in vowels:
+                        z = list(word)
+                        z[i] = a
+                        if len(self.suggestions[word]) < 5 and len(z) > 0 and valid_word("".join(z)) \
+                                and "".join(z) not in self.suggestions[word]:
+                            self.suggestions[word].append("".join(z))
+        else:
+            print("Word must be present in document and have over 3 characters")
+
+    def get_corrections_from_user(self):
+        print("Enter 0 when done")
+        while True:
+            if self.corrections.keys() == self.suggestions.keys():
+                break
+            word = input("Word to be corrected (or 0): ")
+            if word == "0":
+                break
+            if word in self.suggestions.keys():
+                while True:
+                    correction = input("Correction: ")
+                    if correction in self.suggestions[word]:
+                        self.corrections[word] = correction
+                        break
+                    else:
+                        print("Not in suggestions dictionary")
+            else:
+                print("Not in suggestions dictionary")
+
+    def make_corrections(self):
+        c = []
+        with open(self.file, encoding='utf8') as f:
+            for line in f:
+                o = []
+                for word in line.split(" "):
+                    if word in self.corrections.keys():
+                        o.append(self.corrections[word])
+                    elif word.lower() in self.corrections.keys():
+                        o.append(self.corrections[word.lower()].capitalize())
+                    else:
+                        o.append(word)
+                c.append(" ".join(o))
+        with open(self.file, 'w', encoding='utf8') as f:
+            f.truncate()
+            for line in c:
+                f.write(line)
+
     def __str__(self):
         return f'Suggestions:\n' \
-               f'{json.dumps(self.suggestions, indent=len(self.suggestions))}'
+               f'{json.dumps(self.suggestions, indent=len(self.suggestions))}\n' \
+               f'Corrections:\n' \
+               f'{json.dumps(self.corrections, indent=len(self.corrections))}'
